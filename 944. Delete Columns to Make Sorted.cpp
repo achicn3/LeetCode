7e2796b620a4cc927047e6064410e270adec8892
achicn3 <45199813+achicn3@users.noreply.github.com>
2019-02-14 06:11:43 +0800

    Update 944. Delete Columns to Make Sorted.cpp

--- 944. Delete Columns to Make Sorted.cpp
+++ 944. Delete Columns to Make Sorted.cpp
@@ -1,1 +1,17 @@
-Description is shit
+// Problem description is like piece of shit....
+class Solution {
+public:
+    int minDeletionSize(vector<string>& A) {
+        if( A.empty()|| A[0].size()==1 ) return 0;
+        int deleteion=0;
+        for(int i=0;i<A[0].size();i++){
+            for(int j=0;j<A.size()-1;j++){
+                if(A[j][i]>A[j+1][i]){
+                    deleteion++;
+                    break;
+                }
+            }
+        }
+        return deleteion;
+    }
+};
