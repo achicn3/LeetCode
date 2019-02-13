af69449a56734c27314e51f732bcbede7401faa7
achicn3 <45199813+achicn3@users.noreply.github.com>
2019-02-14 04:14:21 +0800

    Create 905. Sort Array By Parity.cpp

---
+++ 905. Sort Array By Parity.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        vector<int> even;
+        vector<int>odd;
+        for(int i=0;i<A.size();i++)
+            if(A[i]%2==0) even.push_back(A[i]);
+            else odd.push_back(A[i]);
+        //append after even 
+        even.insert(even.end(),odd.begin(),odd.end());
+        return even;
+    }
+};
